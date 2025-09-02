0c7ae0b7ba2dd48f4c655cda4e59aea0094275b5
Mahmoudssanad <sanadmahmoud968@gmail.com>
2025-09-02 20:30:11 +0200

    Add: Footer partial view, nav bar in layout and the home page

--- Feed Bridge/obj/Debug/net9.0/Feed Bridge.AssemblyInfo.cs
+++ Feed Bridge/obj/Debug/net9.0/Feed Bridge.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Feed Bridge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfdca548d63b2a0be0c3cf288fc9efc9f242703d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Feed Bridge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Feed Bridge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
