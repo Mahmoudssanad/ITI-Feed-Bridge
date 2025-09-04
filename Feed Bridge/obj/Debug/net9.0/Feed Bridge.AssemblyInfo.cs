989d6a702d4c115b2376ca70b513bfefdfa49cd7
saleh mostafa 11 <120852885+salehmostafa11@users.noreply.github.com>
2025-09-04 17:44:06 +0300

    Revert "DonationDone"
    This reverts commit f1bbff892bb5d49f22fda5743f201845393a78ff.

--- Feed Bridge/obj/Debug/net9.0/Feed Bridge.AssemblyInfo.cs
+++ Feed Bridge/obj/Debug/net9.0/Feed Bridge.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Feed Bridge")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6492f399dfeccd13f3e6c526197b898509c752de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+787e16a782833919373ede006bb920b202d1d60d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Feed Bridge")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Feed Bridge")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
