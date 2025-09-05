e5f398ae33bb0657f11f970768ec5c488d33320b
Mahmoudssanad <sanadmahmoud968@gmail.com>
2025-09-05 22:00:15 +0200

    Add CartService and ICartService

---
+++ Feed Bridge/IServices/ICartService.cs
@@ -0,0 +1,12 @@
+﻿using Feed_Bridge.Models.Entities;
+
+namespace Feed_Bridge.IServices
+{
+    public interface ICartService
+    {
+        //Task AddToCart(string userId, int productId, int quantity);
+        //Task<IEnumerable<Cart>> GetUserCart(string userId);
+        //Task RemoveFromCart(int cartId);
+        //Task ClearCart(string userId);
+    }
+}
